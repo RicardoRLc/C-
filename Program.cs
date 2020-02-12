ed27507b9c772250502db6d4b47a740120d46db3
RICARDO LUIZ DA COSTA <ricardo.costa@alunos.sc.senac.br>
2020-02-11 22:14:00 -0300

    teste

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ricardo.costa.ALUNOS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
